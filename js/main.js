495ed2ce7d26930c628cca47b335eebe8de5e5cc
Skyler Kruger <115855204+skylergkruger@users.noreply.github.com>
2023-01-16 15:18:03 -0800

    Completed render functions

--- js/main.js
+++ js/main.js
@@ -1,66 +1,70 @@
-	/*----- constants -----*/
+/*----- constants -----*/
 
-    const SYMBOLS = {
-        '0' : 'white',
-        '1' : 'purple',
-        '-1' : 'black'
-    };
+const SYMBOLS = {
+    '0': '',
+    '1': 'X',
+    '-1': 'O'
+};
 
-	/*----- state variables -----*/
+/*----- state variables -----*/
 
-    let board;  // array of 3 column arrays
-    let turn; // 1 or -1
-    let winner; // null = no winner, 1 || -1 = winner, 'T' = tie game
+let board;  // array of 3 column arrays
+let turn; // 1 or -1
+let winner; // null = no winner, 1 || -1 = winner, 'T' = tie game
 
-	/*----- cached elements  -----*/
+/*----- cached elements  -----*/
 
-    const messageEl = document.querySelector('h1');
+const messageEl = document.querySelector('h1');
+const playAgainBtn = document.querySelector('button');
 
-	/*----- event listeners -----*/
+/*----- event listeners -----*/
 
 
-	/*----- functions -----*/
+/*----- functions -----*/
 
-    // Initialize all state variables and call render()
+init();
 
-    function init() {
-        board = [
-            [1, 0, 0], // column 0
-            [0, 1, 0], // column 1
-            [0, 1, -1], // column 2
-        ];
-        turn = 1;
-        winner = null;
-        render();
-    }
+// Initialize all state variables and call render()
 
-    // Visualize all state in the DOM
-    function render() {
-        renderBoard();
-        renderMessage();
-        renderControls();
-    }
+function init() {
+    board = [
+        [0, 0, 0], // column 0
+        [0, 0, 0], // column 1
+        [0, 0, 0], // column 2
+    ];
+    turn = 1;
+    winner = null;
+    render();
+}
 
-    function renderBoard() {
-        board.forEach(function(colArr, colIdx) {
-            colArr.forEach(function(cellVal, rowIdx) {
-                const cellId = `${colIdx}r${rowIdx}`;
-                const cellEl = document.getElementById(cellId);
-                cellEl.style.backgroundColor = SYMBOLS[cellVal];
-            });
+// Visualize all state in the DOM
+function render() {
+    renderBoard();
+    renderMessage();
+    renderControls();
+}
+
+function renderBoard() {
+    board.forEach(function (colArr, colIdx) {
+        colArr.forEach(function (cellVal, rowIdx) {
+            const cellId = `c${colIdx}r${rowIdx}`;
+            const cellEl = document.getElementById(cellId);
+            cellEl.innerText = SYMBOLS[cellVal];
         });
-    }
+    });
+}
 
-    function renderMessage() {
-        if (winner === 'T') {
-            messageEl.innerText = "It's a Tie!";
-        } else if (winner) {
-            messageEl.innerHTML = `${SYMBOLS[winner]}`
-        } else {
-            messageEl.innerHTML = `<span style="color: ${SYMBOLS[turn]}>${SYMBOLS[turn].toUpperCase()}'s</span> Turn'`;
-        }
+function renderMessage() {
+    if (winner === 'T') {
+        messageEl.innerText = "It's a Tie!";
+    } else if (winner) {
+        messageEl.innerHTML = `<span style="color: ${SYMBOLS[winner]}"${SYMBOLS[winner].toUpperCase()}</span> Wins!`
+    } else {
+        messageEl.innerHTML = `<span style="color: ${SYMBOLS[turn]}">${SYMBOLS[turn].toUpperCase()}'s</span> Turn`;
     }
+}
 
-    function renderControls () {
+function renderControls() {
+    playAgainBtn.style.visibility = winner ? 'visible' : 'hidden';
 
-    }
+}
