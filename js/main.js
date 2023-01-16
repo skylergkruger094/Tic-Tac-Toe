bc3f3f5196adffef011d758f0a4efea0c36db1d9
Skyler Kruger <115855204+skylergkruger@users.noreply.github.com>
2023-01-16 13:07:05 -0800

    Code init function

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@
 
 	/*----- state variables -----*/
 
+    let board;  // array of 3 column arrays
+    let turn; // 1 or -1
+    let winner; // null = no winner, 1 || -1 = winner, 'T' = tie game
 
 	/*----- cached elements  -----*/
 
@@ -11,3 +14,13 @@
 
 
 	/*----- functions -----*/
+
+    // Initialize all state variables and call render()
+
+    function init() {
+        board = [
+            [0, 0, 0], // column 0
+            [0, 0, 0], // column 1
+            [0, 0, 0], // column 2
+        ];
+    }
