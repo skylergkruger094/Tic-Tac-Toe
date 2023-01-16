52f164698658f2eff6cb64dc446528fbe7ed860e
Skyler Kruger <115855204+skylergkruger@users.noreply.github.com>
2023-01-16 14:04:03 -0800

    Coding render sub-functions

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 	/*----- constants -----*/
 
+    const SYMBOLS = {
+        '0' : 'white',
+        '1' : 'purple',
+        '-1' : 'black'
+    };
 
 	/*----- state variables -----*/
 
@@ -9,6 +14,7 @@
 
 	/*----- cached elements  -----*/
 
+    const messageEl = document.querySelector('h1');
 
 	/*----- event listeners -----*/
 
@@ -19,8 +25,42 @@
 
     function init() {
         board = [
-            [0, 0, 0], // column 0
-            [0, 0, 0], // column 1
-            [0, 0, 0], // column 2
+            [1, 0, 0], // column 0
+            [0, 1, 0], // column 1
+            [0, 1, -1], // column 2
         ];
+        turn = 1;
+        winner = null;
+        render();
+    }
+
+    // Visualize all state in the DOM
+    function render() {
+        renderBoard();
+        renderMessage();
+        renderControls();
+    }
+
+    function renderBoard() {
+        board.forEach(function(colArr, colIdx) {
+            colArr.forEach(function(cellVal, rowIdx) {
+                const cellId = `${colIdx}r${rowIdx}`;
+                const cellEl = document.getElementById(cellId);
+                cellEl.style.backgroundColor = SYMBOLS[cellVal];
+            });
+        });
+    }
+
+    function renderMessage() {
+        if (winner === 'T') {
+            messageEl.innerText = "It's a Tie!";
+        } else if (winner) {
+            messageEl.innerHTML = `${SYMBOLS[winner]}`
+        } else {
+            messageEl.innerHTML = `<span style="color: ${SYMBOLS[turn]}>${SYMBOLS[turn].toUpperCase()}'s</span> Turn'`;
+        }
+    }
+
+    function renderControls () {
+
     }
